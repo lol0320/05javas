276b6842227cef4aba8c03e4f8270b93ee432a56
lol0320 <lol0320@naver.com>
2020-10-15 15:54:37 +0900

    20201015-slide

--- js/31.slide.js
+++ js/31.slide.js
@@ -1,24 +1,62 @@
 new Slide({
 	slide: ['../img/slide-0.jpg', '../img/slide-1.jpg', '../img/slide-2.jpg', '../img/slide-3.jpg'],
+	slideTitle: [
+		'<div class="title">Hello Booldook1</div>',
+		'<div class="title">Hello Booldook2</div>',
+		'<div class="title">Hello Booldook3</div>',
+		'<div class="title">Hello Booldook4</div>'
+	],
 	container: ".main-stage",
 	direction: "hori",	// hori(default), vert, fade
-	autoStart: true,		// false(default), true
 	gapSpeed: 4000,			// 3000(default)
-	aniSpeed: 750,				// 500(default)
+	aniSpeed: 500,			// 500(default)
+	autoUse: true,			// false(default), true
+	btnUse: true,				// true(default), false
+	pagerUse: true			// false(default), true
 });
 
-
-new Slide({
+var slide = new Slide({
 	slide: ['../img/home-2.jpg', '../img/home-3.jpg', '../img/home-4.jpg'],
 	container: ".prd-stage",
-	direction: "vert",	// hori(default), vert, fade
-	autoStart: false,		// false(default), true
+	direction: "vert",
+	autoUse: false,
+	btnUse: true,
+	pagerUse: true
 });
+
+console.log(slide);
 
 new Slide({
 	slide: ['../img/home-2.jpg', '../img/home-3.jpg', '../img/home-4.jpg'],
 	container: ".fade-stage",
-	direction: "fade",	// hori(default), vert, fade
-	autoStart: true,		// false(default), true
+	direction: "fade",
+	autoUse: true,
+	btnUse: true,
+	pagerUse: true
 });
+
+
+new Slide({
+	slide: ['../img/slide-0.jpg', '../img/slide-1.jpg', '../img/slide-2.jpg', '../img/slide-3.jpg', '../img/home-1a.jpg', '../img/home-2.jpg', '../img/home-3.jpg'],
+	slideTitle: [
+		'<div class="title">1</div>',
+		'<div class="title">2</div>',
+		'<div class="title">3</div>',
+		'<div class="title">4</div>',
+		'<div class="title">5</div>',
+		'<div class="title">6</div>',
+		'<div class="title">7</div>'
+	],
+	container: ".step-stage",
+	direction: "step",	// hori(default), vert, fade
+	gapSpeed: 4000,			// 3000(default)
+	aniSpeed: 200,			// 500(default)
+	autoUse: true,			// false(default), true
+	btnUse: true,				// true(default), false
+	pagerUse: true,			// false(default), true
+	slideCnt: 5,				// 5(default)
+	slideMargin: 16			// 0(default), px단위
+});
+
+
 
